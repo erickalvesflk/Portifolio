5e1517e240b9e0f18dbc14278d670898a46065ee
Erick Alves <erickalvesflk@gmail.com>
2026-07-08 17:26:25 -0300

    Descrição dos botões de navegação quando o mouse está em cima

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,29 @@
-import {SkillsCreator} from "./put_skills.js"
+let nav_options = document.querySelectorAll(".nav-option")
 
-async function loadData(address) {
-    const response = await fetch(address)
-    const data = await response.json()
-    
-    return data
+/**
+ * @param {HTMLElement} nav 
+ */
+function showDescription(nav) {
+    let description = nav.querySelector("p")
+    if(description.style.display != "none"){
+        description.style.display = "none"
+    }else{
+        description.style.display = "inline"
+    }
 }
-
-const skills_address = "json/skills.json"
-
-document.addEventListener("DOMContentLoaded", async () => {
-    let myJson = await loadData(skills_address)
-    
-    myJson.forEach((skill, index) => {
-        SkillsCreator.createSkill(
-            skill["skill-name"],
-            skill["level"],
-            skill["date"],
-            skill["projects_quant"],
-            skill["images"],
-            skill["details"]
-        )
-        console.log(`[Skill Loaded] - Skill-name: ${skill["skill-name"]}`)
+document.addEventListener("DOMContentLoaded",()=>{
+    nav_options.forEach((nav, index) => {
+        let description = nav.querySelector("p")
+        console.log(nav)
+        nav.addEventListener("mouseout",() => {
+            console.log(nav)
+            showDescription(nav)
+            description.style.display = "none"
+        })
+        nav.addEventListener("mouseover",() => {
+            console.log(nav)
+            showDescription(nav)
+            description.style.display = "inline"
+        })
     });
 })
